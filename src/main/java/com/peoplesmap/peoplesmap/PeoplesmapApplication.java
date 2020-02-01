0b68edb3a385be8d8337dabd4e0c516e6b219973
Ighor Maia <ighormaia@gmail.com>
2020-02-01 15:56:49 -0300

    initial commit

---
+++ src/main/java/com/peoplesmap/peoplesmap/PeoplesmapApplication.java
@@ -0,0 +1,13 @@
+package com.peoplesmap.peoplesmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplesmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplesmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peoplesmap/peoplesmap/PeoplesmapApplicationTests.java
@@ -0,0 +1,12 @@
+package com.peoplesmap.peoplesmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplesmapApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
